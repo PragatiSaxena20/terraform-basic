c0e35b87aac0d016812ee75fc4cdd1bdd8024a90
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-01-29 13:22:47 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "network_name" {
+  description = "Base Network"
+  value       = var.name
+}
+
+output "network_name" {
+  description = "Network CIDR"
+  value       = var.cidr
+}
+
+output "network_name" {
+  description = "Environment Name"
+  value       = var.environment
+}
