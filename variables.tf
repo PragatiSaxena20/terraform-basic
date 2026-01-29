efe205419ada8915f986883c307eaa930421dc38
PragatiSaxena20 <126959123+PragatiSaxena20@users.noreply.github.com>
2026-01-29 13:19:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "Base Resource"
+  type        = string
+}
+
+variable "cidr" {
+  description = "CIDR block (logical, not cloud-specific)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment  name (dev, stage, prod)"
+  type        = string
+}
